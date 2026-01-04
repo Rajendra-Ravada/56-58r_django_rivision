73e32d99dfb1213a7e9e29cdcadfd94b4a8602a9
Rajendra-Ravada <rajendraravada2004@gmail.com>
2026-01-04 18:56:51 +0530

    deployment purpose

--- revision/basic/views.py
+++ revision/basic/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from django.http import HttpResponse , JsonResponse
 
 # Create your views here.
+
+def home(request):
+    # return  render(request,'home.html')
+    # return HttpResponse("hello world")
+    qp1 = request.GET.get("name","ravi")
+    data = {"student_name": qp1,"age":25,"gender":"male"}
+    return JsonResponse (data , status=200)
+
+
+
+
+movies = [
+
+     {
+        "title": "Baahubali: The Beginning",
+        "year": 2015,
+        "director": "S. S. Rajamouli",
+        "language": "Telugu",
+        "genre": "Action / Fantasy",
+        "rating": 6.0
+    },
+     {
+        "title": "Baahubali: The Conclusion",
+        "year": 2017,
+        "director": "S. S. Rajamouli",
+        "language": "Telugu",
+        "genre": "Action / Fantasy",
+        "rating": 8.2
+    },
+     {
+        "title": "RRR",
+        "year": 2022,
+        "director": "S. S. Rajamouli",
+        "language": "Telugu",
+        "genre": "Action / Drama",
+        "rating": 7.0
+    },
+     {
+        "title": "KGF: Chapter 1",
+        "year": 2018,
+        "director": "Prashanth Neel",
+        "language": "Kannada",
+        "genre": "Action / Crime",
+        "rating": 9.2
+    },
+     {
+        "title": "KGF: Chapter 2",
+        "year": 2022,
+        "director": "Prashanth Neel",
+        "language": "Kannada",
+        "genre": "Action / Crime",
+        "rating": 7.3
+    }
+]
+def movies_list(request):
+    filtered_data = []
+    r1 = float(request.GET.get("r1",6))
+    r2 = float(request.GET.get("r2",8))
+    for movie in movies:
+        if movie["rating"] >r1 and movie["rating"]<r2:
+            filtered_data.append(movie)
+    return JsonResponse({"data":filtered_data})
--- revision/revision/urls.py
+++ revision/revision/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basic.views import home,movies_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home),
+    path('filter/',movies_list)
+
 ]
